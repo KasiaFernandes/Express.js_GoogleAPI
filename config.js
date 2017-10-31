03b7426a8e45698837c8abebab76be74df1ca9f4
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-31 17:54:13 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '138367431680-dhlsj6trk6knivuqrl3qdj2q8cm66m42.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'lLIKSbzqL07i9x0NSmtCk_sl',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
